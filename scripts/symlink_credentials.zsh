00c4410a862f0b2dec7a28349777e8befc91f006
Matthew Benton <m@mattb.tech>
2026-03-29 13:21:34 +0100

    Move ssh config into repo

--- scripts/symlink_credentials.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh -e
-mkdir -p ~/.ssh/
-ln -s /Volumes/private/ssh/config ~/.ssh/config
-ln -s /Volumes/private/ssh/id_rsa ~/.ssh/id_rsa
-ln -s /Volumes/private/ssh/id_rsa.pub ~/.ssh/id_rsa.pub
